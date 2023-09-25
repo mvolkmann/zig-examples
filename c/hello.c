7ca8b5b761bd4c6efeecca2a5653eb9042d1b77b
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-09-24 19:44:45 -0500

    progress

---
+++ c/hello.c
@@ -0,0 +1,7 @@
+// To build this with Zig, enter "zig cc hello.c -o hello"
+#include <stdio.h>
+
+int main() {
+  printf("Hello, World!\n");
+  return 0;
+}
---
+++ c/hello.cpp
@@ -0,0 +1,7 @@
+// To build this with Zig, enter "zig c++ hello.c -o hello"
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
