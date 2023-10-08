68171aa4127f09216e08296a021e2c532816e642
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-10-08 07:43:44 -0500

    progress

--- optional_demo.zig
+++ optional_demo.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const expectEqual = std.testing.expectEqual;
 const print = std.debug.print;
+const String = []const u8;
 
 fn report(wrapper: ?i8) void {
     if (wrapper) |value| {
@@ -15,6 +16,56 @@ pub fn main() void {
     report(wrapper);
     wrapper = 19;
     report(wrapper);
+}
+
+test "optional" {
+    var a: i8 = 0;
+    var b: ?i8 = null;
+    try expectEqual(@as(i8, 0), a);
+    try expectEqual(@as(?i8, null), b);
+
+    a = 1;
+    b = 2;
+    try expectEqual(@as(i8, 1), a);
+    try expectEqual(@as(?i8, 2), b);
+
+    // The orelse operator unwraps the value if non-null
+    // and uses the value that follows if null.
+    // This is why the cast here is to i8 instead of ?i8.
+    try expectEqual(@as(i8, 2), b orelse 0);
+
+    // "b.?" is equivalent to "b orelse unreachable".
+    // It unwraps the value which is why the cast here is to i8 instead of ?i8.
+    try expectEqual(@as(i8, 2), b.?);
+
+    b = null;
+    try expectEqual(@as(?i8, null), b);
+    try expectEqual(@as(i8, 0), b orelse 0);
+    // _ = b.?; // results in "panic: attempt to use null value"
+}
+
+test "dogs" {
+    const Dog = struct {
+        name: ?String = undefined,
+        breed: ?String = undefined,
+    };
+
+    const dog1 = Dog{ .name = "Comet", .breed = "Whippet" };
+    const dog2 = Dog{ .name = "Oscar" };
+    const dog3 = Dog{ .breed = "Beagle" };
+    const dog4 = Dog{};
+    const dogs = [_]Dog{ dog1, dog2, dog3, dog4 };
+
+    for (dogs) |dog| {
+        if (dog.name) |name| print("name = {s}\n", .{name});
+        if (dog.breed) |breed| print("breed = {s}\n", .{breed});
+
+        const present1 = dog.name orelse dog.breed orelse "unknown";
+        print("present1 = {s}\n", .{present1});
+
+        // const present2 = dog.name ?? dog.breed ?? "unknown";
+        // print("present2 = {s}\n", .{present2});
+    }
 }
 
 test "choose" {
