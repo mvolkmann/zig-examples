33c33e6fb1e13b277045975ea03bd47863dcf922
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-10-15 10:55:07 -0500

    progress

--- anytype_demo.zig
+++ anytype_demo.zig
@@ -25,13 +25,16 @@ const Bad = struct {
 fn travelTime(thing: anytype, distance: u32) !f32 {
     // We could use @TypeOf(thing) and functions like
     // std.meta.trait.hasField and std.meta.trait.isIntegral
-    // to verify that thing meets the criteria,
-    // but there is no need to do that because the compiler
-    // will verify that thing has a top_speed field that is an integer
+    // to verify that "thing" meets our criteria.
+    // However, there is no need to do that because the compiler will
+    // verify that "thing" has a "top_speed" field that is an integer
     // just because it is used that way here.
     const s: f32 = @floatFromInt(thing.top_speed);
 
+    // We can't eliminate the local variable d because
+    // @floatFromInt requires that we specify the result type.
     const d: f32 = @floatFromInt(distance);
+
     return d / s;
 }
 
@@ -60,15 +63,15 @@ test "anytype" {
         tolerance,
     );
 
-    // The travelTime function requires the first argument
+    // The travelTime function requires its first argument
     // to be a struct with a top_speed field that is an integer.
 
-    // This results in a compile error, which is good, because
+    // This results in a compile error which is good because
     // the first argument is struct whose top_speed field is not an integer.
     // const bad = Bad{ .top_speed = 1.0 };
     // _ = try travelTime(bad, distance);
 
-    // This results in a compile error, which is good, because
-    // the first argument is a string.
+    // This results in a compile error which is good because
+    // the first argument is not a struct with a "top_speed" field.
     // _ = try travelTime("wrong", distance);
 }
