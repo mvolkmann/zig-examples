4b8cc979d737af85be601832cfe693d5adb30e19
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-10-20 11:01:50 -0500

    progress

--- json_test.zig
+++ json_test.zig
@@ -1,12 +1,29 @@
 const std = @import("std");
-const expectEqual = std.testing.expectEqual;
 const print = std.debug.print;
-const allocator = std.testing.allocator;
+const my_allocator = std.testing.allocator;
+const expectEqual = std.testing.expectEqual;
+const String = []const u8;
 
 const Place = struct {
     lat: f32,
     long: f32,
 };
+
+// T cannot contain any comptime fields, so
+// anonymous structs with numeric values won't work.
+fn fromJSON(T: anytype, allocator: std.mem.Allocator, json: String) !T {
+    const parsed = try std.json.parseFromSlice(T, allocator, json, .{});
+    defer parsed.deinit();
+    return parsed.value;
+}
+
+fn toJSON(allocator: std.mem.Allocator, value: anytype) !String {
+    // The ArrayList that will grow as needed.
+    var out = std.ArrayList(u8).init(allocator); // cannot be const
+    defer out.deinit();
+    try std.json.stringify(value, .{}, out.writer());
+    return try out.toOwnedSlice(); // empties the ArrayList
+}
 
 test "json" {
     const place1 = Place{
@@ -14,17 +31,9 @@ test "json" {
         .long = -0.740687,
     };
 
-    // Convert a struct to a JSON string.
-    var buffer: [100]u8 = undefined;
-    var fbs = std.io.fixedBufferStream(&buffer);
-    try std.json.stringify(place1, .{}, fbs.writer());
-    const json = buffer[0..fbs.pos];
-    print("JSON is {s}\n", .{json});
+    const json = try toJSON(my_allocator, place1);
+    defer my_allocator.free(json);
 
-    // Parse the JSON string to recreate the struct.
-    const parsed = try std.json.parseFromSlice(Place, allocator, json, .{});
-    defer parsed.deinit();
-    const place2 = parsed.value;
-
+    const place2 = try fromJSON(Place, my_allocator, json);
     try expectEqual(place1, place2);
 }
