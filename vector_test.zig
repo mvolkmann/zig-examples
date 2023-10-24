827608f34fb332f70b5c685166302a1a7b51dee0
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-10-23 19:00:10 -0500

    progress

---
+++ vector_test.zig
@@ -0,0 +1,12 @@
+const std = @import("std");
+const expectEqual = std.testing.expectEqual;
+
+test "vector" {
+    // Cannot infer the length using _.
+    const v1 = @Vector(3, f32){ 1.2, 2.3, 3.4 };
+    const v2 = @Vector(3, f32){ 9.8, 8.7, 7.6 };
+    const v3 = v1 + v2;
+    try expectEqual(v3[0], 1.2 + 9.8);
+    try expectEqual(v3[1], 2.3 + 8.7);
+    try expectEqual(v3[2], 3.4 + 7.6);
+}
