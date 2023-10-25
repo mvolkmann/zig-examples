9b5b3fe4cbe23c27cb65c27ccb657165fa8aa6ef
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-10-25 16:59:20 -0500

    progress

--- mylib/src/main.zig
+++ mylib/src/main.zig
@@ -1,10 +1,14 @@
+//! This modules implements math functions.
+
 const std = @import("std");
 const testing = std.testing;
 
+// This adds two integer.
 export fn add(a: i32, b: i32) i32 {
     return a + b;
 }
 
+// This tests the add function.
 test "basic add functionality" {
     try testing.expect(add(3, 7) == 10);
 }
