c7030655abb6f1554c6bf7646fc7400a194320da
R. Mark Volkmann <r.mark.volkmann@gmail.com>
2023-10-25 16:39:26 -0500

    progress

--- orelse_test.zig
+++ orelse_test.zig
@@ -1,6 +1,11 @@
 const std = @import("std");
 const expectEqual = std.testing.expectEqual;
 
+fn double(n: ?i32) i32 {
+    const value = n orelse return 0;
+    return value * 2;
+}
+
 test "orelse" {
     var maybeNumber: ?i32 = null;
     var number = maybeNumber orelse 0;
@@ -9,4 +14,7 @@ test "orelse" {
     maybeNumber = 42;
     number = maybeNumber orelse 0;
     try expectEqual(number, 42);
+
+    try expectEqual(double(2), 4);
+    try expectEqual(double(null), 0);
 }
